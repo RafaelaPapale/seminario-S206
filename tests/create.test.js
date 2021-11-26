78cd6adfd4821bf0c63bac5d28c6b04aef1b61a7
Rafaela de Moraes Papale <rafaela.papale@gec.inatel.br>
2021-11-26 15:25:58 -0300

    Fix errors

--- tests/create.test.js
+++ tests/create.test.js
@@ -1,13 +1,14 @@
 const request = require('supertest');
 
 const CreateObjects = require('./objects/create');
-const SeriesService = require('../src/application/series_service');
+const { mongoose } = require('../src/infrastructure/database');
 const Constants = require('../src/application/utils/constants');
 const app = require('../src/infrastructure/rest_server');
 
 describe('POST / ', () => {
     test('Create Valid Serie - API REST', async () => {
         const response = await request(app).post(`/series/create`).send(CreateObjects.serieValida);
+        mongoose.connection.close();
         expect(response.body.name).toEqual(CreateObjects.serieValida.name);
         expect(response.statusCode).toBe(200);
     });
@@ -16,6 +17,7 @@ describe('POST / ', () => {
 describe('POST / ', () => {
     test('Create An Invalid Serie - API REST', async () => {
         const response = await request(app).post(`/series/create`).send(CreateObjects.serieValida);
+        mongoose.connection.close();
         expect(response.body.name).toEqual(Constants.ErrorDuplicate.name);
         expect(response.statusCode).toBe(400);
     });
